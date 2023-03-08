09e86cb495ebd6b66bb39a65b07d03666e7cb475
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-08 18:51:41 +0300

    added student templates

--- management/students/forms.py
+++ management/students/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
 from professors.models import User, Student
 
 class StudentRegisterForm(UserCreationForm):
@@ -10,13 +11,14 @@ class StudentRegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
 
-
-    def save(self):
+    @transaction.atomic
+    def save(self, *args, **kwargs):
         user = super().save(commit=False)
         user.is_student = True
         user.first_name = self.cleaned_data.get('first_name')
         user.last_name = self.cleaned_data.get('last_name')
         user.email = self.cleaned_data.get('email')
+        user.save() 
         student = Student.objects.create(user=user)
         student.save()
         return user
