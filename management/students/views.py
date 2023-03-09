fb19a5100d65f08d35adb4a7ac3b53977954fe31
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-09 13:05:17 +0300

    student login view

--- management/students/views.py
+++ management/students/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from . models import *
+from . forms import *
 from professors.models import EnrolledCourse, User, Student, Course, test
+from django.views.generic import CreateView
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import  render, redirect
+
 
 # Create your views here.
 
@@ -15,3 +20,12 @@ def students(request):
 
     return render(request, 'students/dashboard.html', {'courses': courses, 'tests': tests, 'activeCourses':activeCourses})
 
+
+class studentReg(CreateView):
+    model = User
+    form_class = StudentRegisterForm
+    template_name = 'students/register.html'
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('login')
