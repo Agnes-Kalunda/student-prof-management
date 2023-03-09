3b0f98c3d712e6d851831ceb55eb686ea59bf9a6
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-09 15:09:57 +0300

    created prof reg, form

--- management/professors/admin.py
+++ management/professors/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import *
+from imghdr import tests
 
 # Register your models here.
+
+admin.site.register(Professor)
+admin.site.register(Student)
+admin.site.register(test)
+admin.site.register(Course)
+admin.site.register(StudentProfile)
+admin.site.register(EnrolledCourse)
---
+++ management/professors/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+from . models import User, Professor
+
+
+
+class ProfessorRegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    # contact = forms.IntegerField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        user.is_professor = True
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.email = self.cleaned_data.get('email')
+        user.save()
+        professor = Professor.objects.create(user=user)
+        # professor.contact = self.cleaned_data.get('contact')
+        professor.save()
+        return user
+
+
+  
