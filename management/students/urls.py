65ae16dfb3526788db4d66374569353865e88163
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-09 13:07:11 +0300

    added student login/registration/dashboard views in urls path

--- management/students/urls.py
+++ management/students/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import  views
+
+
 
 urlpatterns = [
-    path()
+    path ('dashboard/', views.students, name='students'),
+    path('register/', studentReg.as_view(), name='register'),
+    path('login/', views.loginStudent, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
