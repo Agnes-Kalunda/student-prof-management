909f697f46c301a374a1f84e6fdd25e7d65aa436
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-09 13:02:42 +0300

    added student view to display dashboard

--- management/students/views.py
+++ management/students/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from professors.models import EnrolledCourse, User, Student, Course, test
+
 # Create your views here.
 
+
+def students(request):
+    courses = Course.objects.all()
+    tests = test.objects.all()
+    currentUser = request.user.student
+    # activeCourses = get_object_or_404(Course, studentprofile=currentUser.pk)
+    activeCourses = Course.objects.filter(students=currentUser.pk).all()
+
+
+    return render(request, 'students/dashboard.html', {'courses': courses, 'tests': tests, 'activeCourses':activeCourses})
 
