f2bff5f4ddf439c7644bc7c4ca9479dd75c81564
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-09 12:54:57 +0300

    professor/course models

--- management/professors/models.py
+++ management/professors/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
 
 # Create your models here.
 class User(AbstractUser):
-    is_tutor = models.BooleanField(default=False)
+    is_professor = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -17,5 +19,27 @@ class Student(models.Model):
 
     def __str__(self):
         return self.user.first_name
+
+#professorModel 
+class Professor(models.Model):
+    user = models.OneToOneField(User, related_name='professor', on_delete = models.CASCADE, primary_key = True)
+    def __str__(self):
+            return self.user.first_name
     
 
+
+#courseModel
+class Course(models.Model):
+    professor=models.ForeignKey(Professor, on_delete=models.CASCADE)
+    title=models.CharField(max_length=100) 
+    descriptions=models.CharField(max_length=100) 
+    body = models.TextField()
+    coursePoster = models.ImageField(upload_to='coursePoster')
+    category = models.CharField(max_length=100)
+    students= models.ManyToManyField(Student, related_name='enrolls' )
+    # students = models.ForeignKey(Student, related_name='enrolls', on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+            return reverse('professor')
